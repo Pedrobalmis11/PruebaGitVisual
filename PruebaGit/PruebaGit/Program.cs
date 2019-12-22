e6df778491b32bd7888f0d3ac4b4450660062b46
Pedro <pj.martinez@iesdoctorbalmis.com>
2019-12-22 20:09:40 +0100

    Primer Commit

--- PruebaGit/PruebaGit/Program.cs
+++ PruebaGit/PruebaGit/Program.cs
@@ -11,7 +11,7 @@ namespace PruebaGit
         static void Main(string[] args)
         {
             Console.ReadLine();
-            ///sdasdasdsadsasdsd
+            //sdsdsds
 
         }
     }
